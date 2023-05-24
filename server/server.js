be492a074d0ee7947216637cbe529070836be0c2
J2NU <s3773920@rmit.edu.vn>
2023-05-24 22:58:05 +0700

    Updated code of joo branch

--- server/server.js
+++ server/server.js
@@ -32,12 +32,6 @@ app.use(logger);
 
 // Cross Origin Resource Sharing
 app.use(cors(corsOptions));
-/*app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);*/
 
 // built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
